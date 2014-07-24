6fbbf7e99ace82fb0387e2543834fa40c597e0a7
thahirmaheen <Thahir@Thors-MacBook-Pro.local>
2014-07-24 14:09:20 +0530

    pod commit

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LinqToObjectiveC
+#define COCOAPODS_POD_AVAILABLE_LinqToObjectiveC
+#define COCOAPODS_VERSION_MAJOR_LinqToObjectiveC 2
+#define COCOAPODS_VERSION_MINOR_LinqToObjectiveC 0
+#define COCOAPODS_VERSION_PATCH_LinqToObjectiveC 0
+
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 1
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 8
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 1
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 8
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 1
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 8
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 6
+#define COCOAPODS_VERSION_PATCH_SDWebImage 0
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 6
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 0
+
+// objectiveflickr
+#define COCOAPODS_POD_AVAILABLE_objectiveflickr
+#define COCOAPODS_VERSION_MAJOR_objectiveflickr 2
+#define COCOAPODS_VERSION_MINOR_objectiveflickr 0
+#define COCOAPODS_VERSION_PATCH_objectiveflickr 4
+
