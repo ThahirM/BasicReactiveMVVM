3d7ce74b2e121800bcfef6a3eafbe471781554c8
thahirmaheen <Thahir@Thors-MacBook-Pro.local>
2014-07-31 15:55:26 +0530

    TMReactiveView

---
+++ BasicReactiveMVVM/TMReactiveView.h
@@ -0,0 +1,13 @@
+//
+//  TMReactiveView.h
+//  BasicReactiveMVVM
+//
+//  Created by Thahir on 30/07/14.
+//  Copyright (c) 2014 Thahir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TMReactiveView <NSObject>
+- (void)bindViewModel;
+@end
